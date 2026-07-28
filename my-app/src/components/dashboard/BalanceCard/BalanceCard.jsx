fea2de5b33db4213425a31f0bd370d632180b415
PvNastia <nastiapogorelova6@gmail.com>
2026-07-28 18:39:49 +0300

    added a link

--- my-app/src/components/dashboard/BalanceCard/BalanceCard.jsx
+++ my-app/src/components/dashboard/BalanceCard/BalanceCard.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import barIcon from '../../../assets/icons/bar.svg'; 
 import '../../../index.css';
 import "./BalanceCard.css";
 import { setBalance, selectBalance } from '../../../redux/balance/balanceSlice.js';
@@ -48,24 +50,24 @@ const BalanceCard = () => {
         }
     };
 
-    const displayValue = isFocused 
-        ? inputValue 
+    const displayValue = isFocused
+        ? inputValue
         : (currentBalance ? `${Number(currentBalance).toFixed(2)} UAH` : '');
 
     return (
         <div className='balance_rap-component flex'>
             <p className="balance-label">Баланс:</p>
             <div className="input-wrapper">
-                <input 
-                    type="text"  
-                    value={displayValue} 
-                    onChange={handleChange} 
+                <input
+                    type="text"
+                    value={displayValue}
+                    onChange={handleChange}
                     onFocus={() => {
                         setIsFocused(true);
                         setInputValue(currentBalance ? Number(currentBalance).toString() : '');
                     }}
                     onBlur={() => setIsFocused(false)}
-                    placeholder='00.00 UAH' 
+                    placeholder='00.00 UAH'
                 />
                 {showTooltip && (
                     <div className="tooltip-box">
@@ -78,6 +80,10 @@ const BalanceCard = () => {
             <button type="button" className="confirm-btn btn" onClick={handleConfirm}>
                 ПІДВЕРДИТИ
             </button>
+            <Link to="/report" className="report-link">
+                <span>Перейти до розрахунків</span>
+                <img className="report-link-icon" src={barIcon} alt="" />
+            </Link>
         </div>
     );
 };
